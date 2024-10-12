0ee6204fe712c0163ad581edd441c3e09c0ce6c2
Dian4ik <jakie16@yandex.ru>
2024-10-12 23:13:51 +0800

    fix footer mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ import { RiVkLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <footer className="min-h-[420px] bg-gradient-to-b from-background-lightest to-background-light px-16 py-36">
-      <div className="flex justify-between">
-        <div className="flex flex-col gap-28">
+    <footer className="min-h-[420px] bg-gradient-to-b from-background-lightest via-buttons to-button-hover px-4 py-8 sm:px-16 sm:py-36">
+      <div className="flex flex-col gap-y-20 sm:flex-row justify-between">
+        <div className="flex flex-col gap-16 sm:gap-28">
           <div className="flex flex-col gap-8">
-            <h3 className="text-4xl font-serif text-heading-dark tracking-widest">
+            <h3 className="text-3xl sm:text-4xl tracking-widest">
               Мы в соцсетях
             </h3>
-            <div className="flex gap-2 text-heading-dark items-center *:hover:*:opacity-90">
+            <div className="flex gap-2 items-center *:hover:*:opacity-70">
               <Link href={"/"}>
                 <FaInstagram size={28} />
               </Link>
@@ -24,22 +24,24 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-          <div className="text-heading-dark">
+          <div className="">
             <h4 className="mb-4 font-sans-light">
               Подписывайтесь и получайте скидки и бонусы
             </h4>
-            <form className="flex gap-4 items-center">
+            <form className="flex flex-col sm:flex-row gap-2 items-stretch">
               <input
                 type="email"
                 placeholder="E-mail"
-                className="rounded-[14px] py-2 px-4 border border-heading-dark focus:outline-none 
-                placeholder:text-heading-dark text-heading-dark
+                className="rounded-[14px] py-2 px-4 border border-foreground 
+                 placeholder:text-foreground
+                focus:outline-none 
+              
                   font-sans-light"
               />
               <button
-                className="rounded-[14px] bg-button text-heading-dark
-               uppercase font-sans-light tracking-widest border border-heading-dark
-               hover:bg-button-hover
+                className="rounded-[14px] bg-button 
+             tracking-widest border border-foreground
+               hover:bg-transparent hover:italic
                 hover:shadow-[inset_0_0_3px_3px_rgba(43,230,96,0.36),0_0_7px_4px_rgba(43,230,96,0.36)]
                 px-6 py-2"
               >
@@ -48,53 +50,54 @@ const Footer = () => {
             </form>
           </div>
         </div>
-        <div className="flex flex-col gap-8">
-          <h3 className="text-4xl font-serif text-heading-dark tracking-widest">
-            Магазин
-          </h3>
-          <ul className="flex flex-col gap-2 *:hover:*:opacity-60">
-            <li>
-              <Link href={"/"}>Моти</Link>
-            </li>
-            <li>
-              <Link href={"/about"}>Матча</Link>
-            </li>
-            <li>
-              <Link href={"/blog"}>Наборы</Link>
-            </li>
-            <li>
-              <Link href={"/contacts"}>Новинки</Link>
-            </li>
-            <li>
-              <Link href={"/contacts"}>Скидки и акции</Link>
-            </li>
-            <li>
-              <Link href={"/contacts"}>Все продукты</Link>
-            </li>
-          </ul>
+        <div className="flex  sm:gap-14 justify-between">
+          <div className="flex flex-col gap-8">
+            <h3 className="text-3xl sm:text-4xl  tracking-widest">Магазин</h3>
+            <ul className="flex flex-col gap-2 *:hover:*:opacity-60">
+              <li>
+                <Link href={"/"}>Моти</Link>
+              </li>
+              <li>
+                <Link href={"/about"}>Матча</Link>
+              </li>
+              <li>
+                <Link href={"/blog"}>Наборы</Link>
+              </li>
+              <li>
+                <Link href={"/contacts"}>Новинки</Link>
+              </li>
+              <li>
+                <Link href={"/contacts"}>Скидки и акции</Link>
+              </li>
+              <li>
+                <Link href={"/contacts"}>Все продукты</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-8">
+            <h3 className="text-3xl sm:text-4xl tracking-widest">Инфо</h3>
+            <ul className="gap-2 flex flex-col *:hover:*:opacity-60">
+              <li>
+                <Link href={"/"}>Контакты</Link>
+              </li>
+              <li>
+                <Link href={"/"}>FAQ</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Доставка и возврат</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Политика конфиденциальности</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Работа у нас</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex flex-col gap-8">
-          <h3 className="text-4xl font-serif text-heading-dark tracking-widest">
-            Инфо
-          </h3>
-          <ul className="gap-2 flex flex-col *:hover:*:opacity-60">
-            <li>
-              <Link href={"/"}>Контакты</Link>
-            </li>
-            <li>
-              <Link href={"/"}>FAQ</Link>
-            </li>
-            <li>
-              <Link href={"/"}>Доставка и возврат</Link>
-            </li>
-            <li>
-              <Link href={"/"}>Политика конфиденциальности</Link>
-            </li>
-            <li>
-              <Link href={"/"}>Работа у нас</Link>
-            </li>
-          </ul>
-        </div>
+      </div>
+      <div className="text-center mt-16">
+        &copy; 2024 MiDori. Все права защищены.
       </div>
     </footer>
   );
